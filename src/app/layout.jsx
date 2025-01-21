64f562eae23206a7a46d80f848444d868f54c8a2
Thales Henrique <thaalespb@hotmail.com>
2025-01-21 15:19:21 -0300

    Implementando navegação mobile e desktop

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,14 @@
 import { JetBrains_Mono } from "next/font/google";
+import Header from "@/components/Header";
 import "./globals.css";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: '--font-jetbrainsMono'
-})
+  variable: "--font-jetbrainsMono",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -15,13 +18,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${jetbrainsMono.variable}`}
-      >
-        {children}
+      <body className={`${jetbrainsMono.variable}`}>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
 }
-
-
---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { CiMenuFries } from "react-icons/ci";
+import { links } from "./utils";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Thales <span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
