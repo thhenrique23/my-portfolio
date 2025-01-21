28255fbd61b85dbf7968217824d7531f87d3ccc2
Thales Henrique <thaalespb@hotmail.com>
2025-01-21 18:03:12 -0300

    Tela de Resumo

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,7 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { BsArrowDownRight } from "react-icons/bs";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "Backend Developer",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "Logo Design",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+    href: "",
+  },
+];
 const Services = () => {
-  return (
-    <div>Services Page</div>
-  );
+  return <div>Services Page</div>;
 };
 
 export default Services;
